46a71e17a4a7c102f7088385787a9254e4a1eb04
YuanhaoZhu <yuz587@ucsd.edu>
2020-02-19 16:07:28 -0800

    finished ^V^

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.btn').click(function(){
+		ga("send", "event", "like", "click");
+	});
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	//send a boolean var
+	projects["viewAlt"]=false;
+  	response.render('index', projects);
+};
+
+
+
+//create a new controller --> export viewAlt
+exports.viewAlt= function(request, response){
+	//send a boolean var
+	projects["viewAlt"]=true;
   	response.render('index', projects);
 };
